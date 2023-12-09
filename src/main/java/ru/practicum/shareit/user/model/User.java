19dd41ac6676af19f1d7d3052b24c1423e13a13b
Dmitry Miheev <dmiheev@mail.ru>
2023-12-09 13:10:57 +0300

    Sprint 13 part 1 v1.0

---
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -0,0 +1,16 @@
+package ru.practicum.shareit.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class User {
+
+    private long id;
+    private String name;
+    private String email;
+
+}
